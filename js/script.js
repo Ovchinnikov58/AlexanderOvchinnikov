dd0a18ce3e4aa6583ba0cb5a7c8582fba4e4a882
Ovchinnikov58 <h1ttch@yandex.ru>
2020-05-04 17:48:56 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function(){
+    let tab = document.querySelectorAll('.info-header-tab');
+    let info = document.querySelector('.info-header');
+    let tabContent = document.querySelectorAll('.info-tabcontent');
+
+    hideTabContent(1);
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+});
